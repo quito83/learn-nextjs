4cf153bfbf6f2d17afb61c00a779adc3c9f29a19
Quito <quitoapps@gmail.com>
2024-07-19 17:22:30 +0000

    loading 2

--- nextjs-dashboard/app/ui/dashboard/cards.tsx
+++ nextjs-dashboard/app/ui/dashboard/cards.tsx
@@ -17,7 +17,7 @@ export default async function CardWrapper() {
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
-
+{/*
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
@@ -25,7 +25,7 @@ export default async function CardWrapper() {
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      />
+      /> */}
     </>
   );
 }
